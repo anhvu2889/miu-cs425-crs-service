bebc412fb56866db1dfc2462fd088c870c1fea12
Brian <thinhle1199@gmail.com>
2024-07-28 15:41:22 -0500

    resolved conflicts

--- src/main/java/miu/cs425/services/ICarService.java
+++ src/main/java/miu/cs425/services/ICarService.java
@@ -3,6 +3,8 @@ package miu.cs425.services;
 import miu.cs425.models.Car;
 import java.util.List;
 
+import java.util.List;
+
 public interface ICarService {
     Car saveCar(Car car);
 
--- src/main/java/miu/cs425/services/impl/CarService.java
+++ src/main/java/miu/cs425/services/impl/CarService.java
@@ -4,11 +4,9 @@ import miu.cs425.models.Car;
 import miu.cs425.repositories.ICarRepository;
 import miu.cs425.services.ICarService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
-import org.springframework.stereotype.Service;
 
 @Service
 public class CarService implements ICarService {
