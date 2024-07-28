e11170e44ab276b711cbf16bd576910f102e4eff
Brian <thinhle1199@gmail.com>
2024-07-27 23:18:24 -0500

    resolve conflicts

--- src/main/java/miu/cs425/controllers/VehicleController.java
+++ src/main/java/miu/cs425/controllers/VehicleController.java
@@ -1,10 +1,21 @@
 package miu.cs425.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import miu.cs425.models.Car;
+import miu.cs425.services.ICarService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/vehicles")
 public class VehicleController {
 
+    @Autowired
+    private ICarService carService;
+    @PostMapping("/add")
+    public ResponseEntity<Car> addCar (@RequestBody Car car){
+        Car newCar = carService.saveCar(car);
+        return new ResponseEntity<>(newCar, HttpStatus.CREATED);
+    }
 }
---
+++ src/main/java/miu/cs425/services/ICarService.java
@@ -0,0 +1,7 @@
+package miu.cs425.services;
+
+import miu.cs425.models.Car;
+
+public interface ICarService {
+    Car saveCar(Car car);
+}
