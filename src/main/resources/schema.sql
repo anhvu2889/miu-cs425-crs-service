76b238710d178cd494ababaf7733d0a09d2ec188
Brian <thinhle1199@gmail.com>
2024-07-30 10:28:34 -0500

    resolved conflicts

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -112,7 +112,6 @@ CREATE TABLE `users`
   DEFAULT CHARSET = utf8
   COLLATE = utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
 --
 -- Dumping data for table `users`
 --
