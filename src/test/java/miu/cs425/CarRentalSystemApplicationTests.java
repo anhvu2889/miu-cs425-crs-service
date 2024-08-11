cd9abc12094f8e36657174aa6c91fb5f222f8840
vle-miu <lenhat.bkit@gmail.com>
2024-08-11 08:00:36 -0500

    Update Unit test

--- src/test/java/miu/cs425/CarRentalSystemApplicationTests.java
+++ src/test/java/miu/cs425/CarRentalSystemApplicationTests.java
@@ -1,13 +1,12 @@
 package miu.cs425;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class CarRentalSystemApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
